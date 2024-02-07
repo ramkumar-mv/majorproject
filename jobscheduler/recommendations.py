0da8b47f1fc6bffd454361603055125aa3c402f3
Ramkumar M V <86485511+ramkumar-mv@users.noreply.github.com>
2024-02-07 21:17:13 +0530

    Update recommendations.py

--- jobscheduler/recommendations.py
+++ jobscheduler/recommendations.py
@@ -17,7 +17,7 @@ def executeRecommendations():
     # executing empty sample job
     for user in auth.list_users().iterate_all():
         print(user.uid)
-        #thresholdNPK(user.uid)
+        thresholdNPK(user.uid)
         #thresholdHum(user.uid)
 
 
@@ -25,8 +25,8 @@ def thresholdNPK(userId):
     report = ""
 
     #t = date.today().strftime("%Y-%m-%d")
-    data = getNPrevWeek(userId)
-    weeklyTotal = sum([d["total"] for d in data])
+    data = getNToday(userId)
+    '''weeklyTotal = sum([d["total"] for d in data])
 
     data1 = getPPrevWeek(userId)
     weeklyTotal1 = sum([d["total"] for d in data1])
@@ -46,12 +46,12 @@ def thresholdNPK(userId):
     data6 = getRainPrevWeek(userId)
     weeklyTotal6 = sum([d["total"] for d in data6])
 
-    a = weeklyTotal/7
+    a = weeklyTotal/7'''
     report += f"Your Nitrogen value is {data}"
-    report += f"Your Phosphorous value is {data1}"
+    '''report += f"Your Phosphorous value is {data1}"
     report += f"Your Potassium value is {data2}"
     report += f"The Temperature today is {data3} and the humidity today is {data4}"
-    report += f"The ph level is {data5} and the rainfall level is {data6}"
+    report += f"The ph level is {data5} and the rainfall level is {data6}"'''
     #report += f"Predicted crop: {prediction}
     
     '''# Check for NaN or None values in the data
